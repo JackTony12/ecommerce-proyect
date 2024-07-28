8fd3fcc84563eedee9736c3e71264e9cd4a3994b
Antony Leon <aleonelgranados@gmail.com>
2024-07-28 19:47:20 +0200

    v1.1 correccion del logOut redireccion a pagina incorrecta, add toast singIn, logOut

--- src/components/products/products.jsx
+++ src/components/products/products.jsx
@@ -4,61 +4,53 @@ import { FaCartPlus, FaCheck } from 'react-icons/fa'
 import { Toaster, toast } from 'sonner'
 import loadingImg from '../../assets/img/loading.jpg'
 const Products = ({ productos }) => {
-  const URL = `https://dummyjson.com${productos}`
-  const { data, isLoading, setUrl, handleAddProduct } = useContext(MyStore)
-  useEffect(() => {
-    setUrl(URL)
-    console.log(URL)
-  }, [URL, setUrl])
+ const URL = `https://dummyjson.com${productos}`
+ const { data, isLoading, setUrl, handleAddProduct } = useContext(MyStore)
+ useEffect(() => {
+  setUrl(URL)
+ }, [URL, setUrl])
 
-  return (
-    <>
-      {!!isLoading && <h1>is loading...</h1>}
+ return (
+  <>
+   {!!isLoading && <h1>is loading...</h1>}
 
-      <div className="product-container">
-        {data &&
-          data.products.map((p) => {
-            const shortTItle = p.title.split(' ').slice(0, 2).join(' ')
-            return (
-              <div
-                data-aos="fade-up"
-                className="product-card-container"
-                key={p.id}
-              >
-                <div className="product-img-container">
-                  <img
-                    src={isLoading ? loadingImg : p.images[0]}
-                    alt="ProductImg"
-                  />
-                </div>
-                <div className="product-description">
-                  <h3>{shortTItle}</h3>
-                  <p>${p.price}</p>
-                  <p>{p.category}</p>
-                  <div>
-                    <button
-                      onClick={() => {
-                        handleAddProduct({
-                          price: p.price,
-                          name: shortTItle,
-                          id: p.id,
-                        })
-                        toast('Producto agregado', {
-                          icon: <FaCheck style={{ color: 'green' }} />,
-                        })
-                      }}
-                    >
-                      Añadir <FaCartPlus />
-                    </button>
-                  </div>
-                </div>
-              </div>
-            )
-          })}
-      </div>
-      <Toaster />
-    </>
-  )
+   <div className="product-container">
+    {data &&
+     data.products.map((p) => {
+      const shortTItle = p.title.split(' ').slice(0, 2).join(' ')
+      return (
+       <div data-aos="fade-up" className="product-card-container" key={p.id}>
+        <div className="product-img-container">
+         <img src={isLoading ? loadingImg : p.images[0]} alt="ProductImg" />
+        </div>
+        <div className="product-description">
+         <h3>{shortTItle}</h3>
+         <p>${p.price}</p>
+         <p>{p.category}</p>
+         <div>
+          <button
+           onClick={() => {
+            handleAddProduct({
+             price: p.price,
+             name: shortTItle,
+             id: p.id,
+            })
+            toast('Producto agregado', {
+             icon: <FaCheck style={{ color: 'green' }} />,
+            })
+           }}
+          >
+           Añadir <FaCartPlus />
+          </button>
+         </div>
+        </div>
+       </div>
+      )
+     })}
+   </div>
+   <Toaster />
+  </>
+ )
 }
 
 export default Products
--- src/firebase/singnin.jsx
+++ src/firebase/singnin.jsx
@@ -1,8 +1,11 @@
 import React, { useEffect, useState } from 'react'
 import { auth, provider } from './firebaseCredentials'
 import { signInWithPopup } from 'firebase/auth'
-import { FaUser } from 'react-icons/fa'
+import { FaUser, FaCheck } from 'react-icons/fa'
+import { CiWarning } from 'react-icons/ci'
 import { TbLogout2 } from 'react-icons/tb'
+import { toast, Toaster } from 'sonner'
+import { useNavigate } from 'react-router-dom'
 import './style.css'
 
 function SingIn() {
@@ -11,17 +14,16 @@ function SingIn() {
   signInWithPopup(auth, provider).then((data) => {
    setValue(data.user.email)
    localStorage.setItem('email', data.user.displayName)
-   console.log(data.user)
+   toast('Ha iniciado sesión', { icon: <FaCheck style={{ color: 'green' }} /> })
   })
  }
  useEffect(() => {
   setValue(localStorage.getItem('email'))
   if (localStorage.getItem) {
-   console.log(value)
   }
-  console.log(value)
  }, [])
 
+ const navigate = useNavigate()
  return (
   <div>
    <button
@@ -37,14 +39,18 @@ function SingIn() {
     onClick={() => {
      if (localStorage.getItem('email')) {
       localStorage.clear()
-      window.location.reload()
      }
+     navigate('/')
+     toast('Ha salido de su cuenta', {
+      icon: <CiWarning style={{ color: 'red' }} />,
+     })
     }}
     className={`log-uot ${localStorage.getItem('email') ? 'show' : 'hidde'}`}
    >
     <TbLogout2 />
     Log out
    </button>
+   <Toaster />
   </div>
  )
 }
